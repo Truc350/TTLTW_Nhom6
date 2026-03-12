f8515077f92fb48ca7a5b2d487ef896a7645d937
Truc350 <23130350@st.hcmuaf.edu.vn>
2026-03-12 14:59:36 +0700

    first commit

---
+++ src/main/java/vn/edu/hcmuaf/fit/ttltw_nhom6/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.edu.hcmuaf.fit.ttltw_nhom6;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
